04acc75f3bfd9fd48b78453e70526d3731098fab
Yeongrok Song <song7868@gmail.com>
2025-05-27 00:01:17 +0900

    fix: settings.py 변경

--- graduateCheck/graduateCheck/settings.py
+++ graduateCheck/graduateCheck/settings.py
@@ -35,13 +35,11 @@ ALLOWED_HOSTS = ['kuphilgraduate.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
-    # "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "django_extensions",
     "myapp",
 ]
 
